284700ea553b52e27bed0f58f6cc059bfbf9096b
oshiba777 <stik_xi@mail.ru>
2025-09-12 12:40:57 +0500

    init project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.card');//выбираем все элементы с классом card на странице
+for (let card of cards){
+    card.addEventListener('click', ()=>{
+    card.classList.toggle('card-show')
+});
+}
+
+
