7e7bbd827712b50e294451ef5eb142eb8f89c254
Студенческая <@rbiu.local>
2025-10-03 11:15:26 +0500

    working sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 import autoprefixer from "autoprefixer";
 import { plugin } from "postcss";
 
-export default{
-    plugin:{
-        autoprefixer:{
-            overrideBrowserslist:[">0.1%", 'last 2 version', 'nod dead']
-        }
-    }
-}
+ export default {
+      plugins: {
+        autoprefixer: {
+          overrideBrowserslist: ['> 0.1%', 'last 2 versions', 'not dead'],
+        },
+      },
+    };
